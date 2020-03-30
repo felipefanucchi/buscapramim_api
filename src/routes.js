63692ca2ddbb7e76c41fe56dd7f0f738df1e000b
Felipe Fanucchi <ffanucchi@studiovisual.com.br>
2020-03-29 22:51:11 -0300

    Retornando body e headers para fins d teste

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const db = require('./database/db');
 
 // Controllers
-router.get('/', (request, response) => {
-  const {body} = request;
-  return response.json({ body: body.teste });
+router.get('/', async (request, response) => {
+  const {name, description} = request.body;
+  const {user_id} = request.headers;
+
+  const user = {
+    name,
+    description,
+    user_id: parseInt(user_id)
+  };
+
+  return response.json({ user });
 });
 
 module.exports = router;
