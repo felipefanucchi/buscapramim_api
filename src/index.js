395b9798ed3bd53f565163574e47e165c3e59799
Filipe Souza <filipe.souza@weef.com.br>
2020-03-30 19:52:40 -0300

    - Adicionado dotenv no arquivo index.js para usar a porta especificada no arquivo env, ou a porta padrao; - Adicionada documentação basica; - Modificado docker-compose para usar o postgis;

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const routes = require('./routes');
+const dotenv = require('dotenv');
+
+dotenv.config({path: __dirname+'/.env'});
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.API_PORT | 3333);
