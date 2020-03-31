c0eb6932987b0a9b9631cc028ff6ff9c7fd3aa21
Felipe Fanucchi <ffanucchi@studiovisual.com.br>
2020-03-31 10:39:17 -0300

    Adicionando Joi ao projeto criando schema para validacao dos dados e alterando forma de envio d coordenadas para um array

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -2,6 +2,21 @@ const db = require('../database/db');
 const knexPostgis = require('knex-postgis');
 const st = knexPostgis(db);
 
+// validation
+const Joi = require('@hapi/joi');
+const customJoi = Joi.extend(require('joi-phone-number'));
+
+const schema = {
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+  phone: customJoi.string().phoneNumber().validate('+32494567324'),
+  coordinates: Joi.array().ordered(
+    Joi.number().min(-90).max(90).required(),
+    Joi.number().min(-180).max(180).required()
+  ).description("Please use this format [ longitude, latitude ]"),
+};
+
 module.exports = {
   async create(request, response) {
     const {
@@ -9,9 +24,10 @@ module.exports = {
       email,
       phone,
       password,
-      latitude,
-      longitude
+      coordinates
     } = request.body;
+
+    const [longitude, latitude] = coordinates;
 
     try {
       await db('users').insert({
@@ -22,8 +38,23 @@ module.exports = {
         coordinates: st.setSRID(st.geomFromText(`Point(${longitude} ${latitude})`), 4326)
       });
 
-      const coordinates = await db('users').select('users.name', st.x('coordinates').as('longitude'), st.y('coordinates').as('latitude'));
-      return response.json({coordinates});
+      let users = await db('users').select([
+        'users.name',
+        'users.email',
+        'users.phone',
+        st.x('coordinates').as('longitude'),
+        st.y('coordinates').as('latitude')
+      ]);
+
+      users = users.map(user => { 
+        user.coordinates = [ user.longitude, user.latitude ];
+        delete user.longitude;
+        delete user.latitude;
+
+        return user;
+      });
+
+      return response.json({users});
     } catch(err) {
       return response.status(400).send(err);
     }
