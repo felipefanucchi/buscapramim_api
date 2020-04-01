38085aa1e272ce60e84a875085a7f100411285a6
Felipe Fanucchi <ffanucchi@studiovisual.com.br>
2020-03-31 22:25:00 -0300

    Cria validacao para email ja caadastrado

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -1,26 +1,13 @@
 const db = require('../database/db');
 const knexPostgis = require('knex-postgis');
 const st = knexPostgis(db);
-
-// validation
-const Joi = require('@hapi/joi');
-
-const schema = Joi.object({
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required(),
-  phone: Joi.string().min(10).max(11),
-  coordinates: Joi.array().ordered(
-    Joi.number().min(-90).max(90).required(),
-    Joi.number().min(-180).max(180).required()
-  ).description("Please use this format [ longitude, latitude ]"),
-});
+const { registerValidation } = require('../validation');
 
 module.exports = {
   async create(request, response) {
-    const { error } = schema.validate(request.body);
+    const { error } = registerValidation(request.body);
 
-    if (error) return response.status(400).json({error: error.details[0].message});
+    if (error) return response.status(400).json({error});
 
     const {
       name,
@@ -31,6 +18,15 @@ module.exports = {
     } = request.body;
 
     const [longitude, latitude] = coordinates;
+
+    const emailExists = await db('users')
+      .where('users.email', email)
+      .select('*')
+      .first();
+
+    console.log(emailExists);
+
+    if (emailExists) response.status(400).json({error: 'E-mail already registered.'});
 
     try {
       await db('users').insert({
---
+++ src/validation.js
@@ -0,0 +1,30 @@
+const Joi = require('@hapi/joi');
+
+const registerValidation = data => {
+  const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+    phone: Joi.string().min(10).max(11),
+    coordinates: Joi.array().ordered(
+      Joi.number().min(-90).max(90).required(),
+      Joi.number().min(-180).max(180).required()
+    ).description("Please use this format [ longitude, latitude ]"),
+  });
+
+  console.log(schema.validate(data));
+
+  return schema.validate(data);
+};
+
+const loginValidation = data => {
+  const schema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+
+  return schema.validate(data);
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
