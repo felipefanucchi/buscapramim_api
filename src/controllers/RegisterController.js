b480583d056116befe2758f8f90ec5f6e9f4d6d6
Felipe Fanucchi <ffanucchi@studiovisual.com.br>
2020-03-31 22:03:22 -0300

    Adiciona validacao joi na request body

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -4,21 +4,24 @@ const st = knexPostgis(db);
 
 // validation
 const Joi = require('@hapi/joi');
-const customJoi = Joi.extend(require('joi-phone-number'));
 
-const schema = {
+const schema = Joi.object({
   name: Joi.string().min(6).required(),
   email: Joi.string().min(6).required().email(),
   password: Joi.string().min(6).required(),
-  phone: customJoi.string().phoneNumber().validate('+32494567324'),
+  phone: Joi.string().min(10).max(11),
   coordinates: Joi.array().ordered(
     Joi.number().min(-90).max(90).required(),
     Joi.number().min(-180).max(180).required()
   ).description("Please use this format [ longitude, latitude ]"),
-};
+});
 
 module.exports = {
   async create(request, response) {
+    const { error } = schema.validate(request.body);
+
+    if (error) return response.status(400).json({error: error.details[0].message});
+
     const {
       name,
       email,
@@ -38,23 +41,23 @@ module.exports = {
         coordinates: st.setSRID(st.geomFromText(`Point(${longitude} ${latitude})`), 4326)
       });
 
-      let users = await db('users').select([
-        'users.name',
-        'users.email',
-        'users.phone',
-        st.x('coordinates').as('longitude'),
-        st.y('coordinates').as('latitude')
-      ]);
+      // let users = await db('users').select([
+      //   'users.name',
+      //   'users.email',
+      //   'users.phone',
+      //   st.x('coordinates').as('longitude'),
+      //   st.y('coordinates').as('latitude')
+      // ]);
 
-      users = users.map(user => { 
-        user.coordinates = [ user.longitude, user.latitude ];
-        delete user.longitude;
-        delete user.latitude;
+      // users = users.map(user => { 
+      //   user.coordinates = [ user.longitude, user.latitude ];
+      //   delete user.longitude;
+      //   delete user.latitude;
 
-        return user;
-      });
+      //   return user;
+      // });
 
-      return response.json({users});
+      return response.status(204);
     } catch(err) {
       return response.status(400).send(err);
     }
