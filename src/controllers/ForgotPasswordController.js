37704a3d71b79ee2addaff3ef20bf50aee188562
Filipe Souza <filipemanueldesouza@hotmail.com>
2020-04-02 11:49:07 -0300

    - Adicionada dependencia para gerar documentacao da API com o swagger; - Modificado o arquivo de configuracao do servidor para inserir as configuracoes do swagger-express-generator; - Modificado docker-compose para adicionar novos servicos, como o pgAdmin4 e o Mailhog; - Modificado o arquivo .env.example para ser mais compativel com o uso com o docker e remover senhas; - Modificado o controller de esqueci a senha para insercao da documentacao de exemplo da rota; - Atualizado o README para informar sobre a documentacao da API;

--- src/controllers/ForgotPasswordController.js
+++ src/controllers/ForgotPasswordController.js
@@ -1,8 +1,18 @@
+require('dotenv').config({path: __dirname+'/.env'});
 const db = require('../database/db');
 const crypto = require('crypto');
 const mailer = require('../modules/mailer');
 const { forgotPasswordValidation } = require('../validation');
 
+/**
+ * @route POST /forgot_password
+ * @group user - Operações para usuários
+ * @param {string} email.body.required - E-mail do usuário - Exemplo: a@a.com.br
+ * @type {{create(*, *): Promise<*|undefined>}}
+ * @returns {object} 400 - Um erro ao tentar enviar o email.
+ * @returns {object} 204 - O email foi enviado.
+ * @returns {object} 200 - Ocorreu um erro ao tentar redefinir a senha.
+ */
 module.exports = {
   async create(request, response) {
     const { error } = forgotPasswordValidation(request.body);
@@ -15,7 +25,7 @@ module.exports = {
       const user = await db('users')
         .where('users.email', email)
         .select('*')
-        .first(); 
+        .first();
 
       if (!user) return response.status(400).send({error: 'This email does not exist in our database.'});
 
@@ -33,7 +43,7 @@ module.exports = {
 
       await mailer.sendMail({
         to: email,
-        from: 'flfanucchi@gmail.com',
+        from: process.env.MAILER_FROM_EMAIL,
         template: '../mail/forgot-password',
         context: { token },
       }, err => {
@@ -47,4 +57,4 @@ module.exports = {
       return response.json({error: 'Error on forgot password, try again!'});
     }
   }
-}
+};
--- src/index.js
+++ src/index.js
@@ -3,11 +3,40 @@ const app = express();
 const cors = require('cors');
 const routes = require('./routes');
 const dotenv = require('dotenv');
+const expressSwagger = require('express-swagger-generator')(app);
 
 dotenv.config({path: __dirname+'/.env'});
+
+let swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      description: "Documentação dos endpoints de Busca pra mim",
+      title: "Busca pra mim API",
+      version: '1.0.0',
+    },
+    host: 'localhost:'+process.env.API_PORT | 3333,
+    basePath: 'api/v1',
+    produces: [
+      "application/json",
+    ],
+    schemes: ['http', 'https'],
+    securityDefinitions: {
+      JWT: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'Authorization',
+        description: "Bearer <token>",
+      }
+    }
+  },
+  basedir: __dirname,
+  files: ['./controllers/**/*.js']
+};
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+expressSwagger(swaggerOptions);
 
 app.listen(process.env.API_PORT | 3333);
